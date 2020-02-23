16956fc02b171fa0a18da0ddeb1c73d9deeb1b2a
gabrielcaballerosta <gabri.vecindario.2011@gmail.com>
2020-02-23 12:38:43 +0100

    Initial commit

---
+++ src/test/java/com/study/gcs/Payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.gcs.Payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
